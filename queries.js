48a411e84f91b61a160aec8af9a00788d5fef307
shreyashfederer <shreyash.bukkawar@peak.ai>
2021-08-02 11:03:28 +0530

    Added update book by book id endpoint

--- queries.js
+++ queries.js
@@ -6,12 +6,13 @@ const getUsers = (request, response) => {
 
         if (error) {
 
+
             if (error.toString().includes("password authentication failed")) {
                 response.status(401).send(`password authentication failure`)
                 console.log(error.toString())
                 return
             }
-            
+
 
         } else {
             response.status(200).json(results.rows)
@@ -26,16 +27,20 @@ const getUserById = (request, response) => {
 
     const id = parseInt(request.params.id)
 
-    db.pool.query('SELECT * from books where id = $1 ',[id] ,(error, results) => {
+    db.pool.query('SELECT * from books where id = $1 ', [id], (error, results) => {
 
-        if (error) {
+        if (results.rows.length == 0) {
+            response.status(404).send(`book doesn't exists with given id`)
+        }
+        else if (error) {
 
+            //console.log(error.toString())
             if (error.toString().includes("password authentication failed")) {
                 response.status(401).send(`password authentication failure`)
                 console.log(error.toString())
                 return
             }
-            
+
 
         } else {
             response.status(200).json(results.rows)
@@ -73,8 +78,44 @@ const createUser = (request, response) => {
 
 }
 
+const updateUser = (request, response) => {
+
+    const id = parseInt(request.params.id)
+
+    const { author, title } = request.body
+
+    db.pool.query("Update books SET author = $1,title=$2 WHERE id = $3", [author, title, id], (error, results) => {
+
+        if (results.rowCount == 0) {
+            response.status(404).send(`book doesn't exists with given id`)
+        }
+       
+        else if (error) {
+
+            if (error.toString().includes("password authentication failed")) {
+                response.status(401).send(`password authentication failure`)
+                console.log(error.toString())
+
+            } else if (error.toString().includes("null value in column")) {
+
+                response.status(400).send(error.toString())
+                console.log(error.toString())
+
+            }
+
+
+        } else {
+            response.status(200).send(`User Updated successfully `)
+        }
+
+
+    })
+
+}
+
 module.exports = {
     getUsers,
     createUser,
-    getUserById
+    getUserById,
+    updateUser
 }
